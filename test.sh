5a2a72bc16d54590026355b8d63b8ce0d70757d9
rduser <rduser@gmail.com>
2021-10-08 21:10:28 +0800

    大哥沒有輸

--- test.sh
+++ test.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 k=$( awk 'END{print NR}' test.csv)
 echo $k
-#for i in $(cat $k)
-   # do
-				
-        #echo kk
 
-    #done
+for i in $(1..$k)
+p=$(cat test.csv | awk -F, 'NR==i{print $2}')
+    #do
+echo      - targets: ['192.168.168.128:$p']
+        #labels:
+          #code: 1-1
+          #port: 1-2
+          #slug: 1-3		
+
+
