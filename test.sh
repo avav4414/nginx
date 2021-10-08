62cf26086ee8820906b4f831d25ef9f41e74c0d6
rduser <rduser@gmail.com>
2021-10-08 23:33:41 +0800

    test end

--- test.sh
+++ test.sh
@@ -9,9 +9,13 @@ for ((i=1;i<=3;i++))
 do
 #echo $i
 s=$(cat test.csv | awk -F, 'NR=='$i'{print $2}')
-
+g=$(cat test.csv | awk -F, 'NR=='$i'{print $1}')
+p=$(cat test.csv | awk -F, 'NR=='$i'{print $3}')
 echo - targets: ['192.168.168.128:'$s'']
-
+echo    labels:
+echo      code: ''$g''
+echo      port: ''$s''
+echo      slug: ''$p''
 done
 
 #echo      - targets: ['192.168.168.128:$p']
