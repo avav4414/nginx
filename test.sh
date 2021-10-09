0ec28c449586d2766c45f933e46c1f229d998734
rduser <rduser@gmail.com>
2021-10-09 22:30:16 +0800

    pull

--- test.sh
+++ test.sh
@@ -2,15 +2,18 @@
 k=$( awk 'END{print NR}' test.csv)
 
 
-#for i in {1..$k}
+
 for ((i=1;i<=$k;i++))
 
 
 do
 
 s=$(cat test.csv | awk -F, 'NR=='$i'{print $2}')
+
 g=$(cat test.csv | awk -F, 'NR=='$i'{print $1}')
+
 p=$(cat test.csv | awk -F, 'NR=='$i'{print $3}')
+
 echo "      "-" "targets:" "[\'192.168.168.128:''$s''\']
 echo "        "labels:
 echo "          "code:" "\'''$g''\'
@@ -21,11 +24,5 @@ done
 
 
 
-#tee ls.txt
-#echo      - targets: ['192.168.168.128:$p']
-        #labels:
-          #code: 1-1
-          #port: 1-2
-          #slug: 1-3		
 
 
