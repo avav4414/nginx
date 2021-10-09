c1af9437720fb891cbe86cd51fdc6e765efea4eb
rduser <rduser@gmail.com>
2021-10-09 15:49:55 +0800

    test109

--- test.sh
+++ test.sh
@@ -13,7 +13,7 @@ g=$(cat test.csv | awk -F, 'NR=='$i'{print $1}')
 p=$(cat test.csv | awk -F, 'NR=='$i'{print $3}')
 echo - targets: ['192.168.168.128:'$s'']
 echo    labels:
-echo      code: ''$g''
+echo      "  "code: ''$g''
 echo      port: ''$s''
 echo      slug: ''$p''
 done
